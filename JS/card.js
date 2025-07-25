f685e23b989f64b6a69c8a03fee3f677c92badf6
DhGilGar <110663176+DhGilGar@users.noreply.github.com>
2025-07-25 15:54:07 +0200

    Add files via upload

---
+++ JS/card.js
@@ -0,0 +1,12 @@
+  function expandCard(card) {
+    const cardContainer = card.closest(".card-container");
+    card.classList.toggle("open");
+
+    if (card.classList.contains("open")) {
+      cardContainer.classList.remove("col-md-3");
+      cardContainer.classList.add("col-md-12");
+    } else {
+      cardContainer.classList.remove("col-md-12");
+      cardContainer.classList.add("col-md-3");
+    }
+  }
