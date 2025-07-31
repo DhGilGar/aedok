4efdedef75ead4f0e153816cc16cae895d796386
DhGilGar <110663176+DhGilGar@users.noreply.github.com>
2025-07-31 11:41:16 +0200

    Add files via upload

--- JS/card.js
+++ JS/card.js
@@ -1,17 +1,18 @@
-  function expandCard(card) {
-    const cardContainer = card.closest(".card-container");
-    card.classList.toggle("open");
+ 
+function expandCard(card) {
+  const cardContainer = card.closest(".card-container");
+  card.classList.toggle("open");
 
-    if (card.classList.contains("open")) {
-      cardContainer.classList.remove("col-md-3");
-      cardContainer.classList.add("col-md-12");
-    } else {
-      cardContainer.classList.remove("col-md-12");
-      cardContainer.classList.add("col-md-3");
-    }
+  if (card.classList.contains("open")) {
+    cardContainer.classList.remove("col-md-3");
+    cardContainer.classList.add("col-md-12");
+  } else {
+    cardContainer.classList.remove("col-md-12");
+    cardContainer.classList.add("col-md-3");
   }
+}
 
-  document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".card.card-person").forEach(card => {
     card.addEventListener("click", () => expandCard(card));
   });
